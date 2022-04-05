bade9f276e593fe3c1d7a867740b8b21c9db588a
stniot1 <stniot1@d11-stniot1.vdc.saab.se>
2022-04-05 11:35:03 +0200

    update requst to database

--- sslapp/func_check.py
+++ sslapp/func_check.py
@@ -4,25 +4,27 @@ from .models import sslSiteModel
 
 
 def listModel():
-    data = sslSiteModel.objects.values_list('id','ssl_site')
-    print (data.id)
-    for da in sslSiteModel.objects.values_list():
-        print (da.id)
+    data = sslSiteModel.objects.all()
+    
+    for da in data:
+        print (da.ssl_site)
 
 def run_sslcheckModule():
     url_check = "status.python.org"
+    data = sslSiteModel.objects.all()
     
-    
-    # output as bytes
-    output = subprocess.run(["sslcheck",url_check], capture_output=True)
-    
-    # Convert output utf-8
-    output_str = output.stdout.decode('utf-8')
-    print(output_str)
-    print("---------------")
-    listModel()
-    
-    
+    for url_req in data:
+        
+        # output as bytes
+        output = subprocess.run(["sslcheck",url_req.ssl_site], capture_output=True)
+        
+        # Convert output utf-8
+        #output_str = output.stdout.decode('utf-8')
+        print(output)
+        print(type(output))
+        
+        #Save to DB
+        sslSiteModel.objects.filter(pk=url_req.id).update(ssl_cert_info=output)
             
 #if __name__ == '__main__':
 #     run_sslcheckModule(url_check)
