5e80a0effdeed007af450b88bfd801d2fcb1b801
stniot1 <stniot1@d11-stniot1.vdc.saab.se>
2022-04-06 16:41:36 +0200

    search status & expired date

--- sslapp/func_check.py
+++ sslapp/func_check.py
@@ -19,12 +19,31 @@ def run_sslcheckModule():
         output = subprocess.run(["sslcheck",url_req.ssl_site], capture_output=True)
         
         # Convert output utf-8
-        #output_str = output.stdout.decode('utf-8')
-        print(output)
-        print(type(output))
+        output_str = output.stdout.decode('utf-8')
+        #print(len(output_str))
+        print(output_str)
+        #print(type(output_str))
         
+        # search for expire date
+        search_string = 'expire on'
+        #print (output_str.find(search_string,100,500))
+        i = len(search_string) + output_str.find(search_string,100,500) + 1
+        x = i + 10
+        s_date = ''
+        while i < x:
+           # print (output_str[i])
+            s_date = s_date + output_str[i] 
+            i += 1
+        
+        print (s_date)
+        
+        s_status = 0
+        if "is ok" in output_str:
+            s_status = 1
+
         #Save to DB
-        sslSiteModel.objects.filter(pk=url_req.id).update(ssl_cert_info=output)
+        sslSiteModel.objects.filter(pk=url_req.id).update(ssl_status=s_status)
+        sslSiteModel.objects.filter(pk=url_req.id).update(ssl_cert_info=output_str)
             
 #if __name__ == '__main__':
 #     run_sslcheckModule(url_check)
